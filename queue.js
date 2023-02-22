40400db26df4e77985ab70fe6f6668de46de919c
JUNG SUNG YOON <xbox1111@naver.com>
2023-02-23 02:05:24 +0900

    commit

---
+++ queue.js
@@ -0,0 +1,46 @@
+class Queue {
+  constructor() {
+    this.storage = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+  
+  size() {
+    if (this.storage[this.rear] === undefined) {
+      return 0;
+    } else {
+      return this.rear - this.rear + 1;
+    }
+  }
+  
+  add(value) {
+    if (this.size() === 0) {
+      this.storage['0'] = value;
+    } else {
+      this.rear += 1;
+      this.storage[this.rear] = value;
+    }
+  }
+  
+  popleft() {
+    let temp;
+    if (this.front === this.rear) {
+      temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front = 0;
+      this.rear = 0;
+    } else {
+      temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front += 1;
+    }
+    return temp;
+  }
+}
+
+const queue = new Queue()
+
+queue.add(1)
+queue.add(3)
+
+console.log(queue.size())
